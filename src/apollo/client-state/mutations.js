66a8eb12804d11f37a4b72f6acc0b8f7f6c3264e
Brendan Asselstine <mail.asselstine@gmail.com>
2019-02-06 12:04:17 -0800

    Removed getAllTransactionsByPackageId
    Refactored the VouchMutationForm so that it would use a tx prop instead of multiple attributes
    
    Completed ZepToken approval functionality

--- src/apollo/client-state/mutations.js
+++ src/apollo/client-state/mutations.js
@@ -10,111 +10,117 @@ export const mutations = {
   resolvers: {
     Mutation: {
       sendTransaction: async (_, variables, { cache, getCacheKey }) => {
-        const { method, args } = variables.txData
-        await getMetamaskPermissions()
-        const provider = new ethers.providers.Web3Provider(window.ethereum)
-        const network = await provider.getNetwork()
-        const signer = provider.getSigner()
-        const address = abiMapping.getAddress('Vouching', network.chainId)
-        const abi = abiMapping.getAbi('Vouching')
-        const contract = new ethers.Contract(address, abi, signer)
-
-        const methodFxn = contract[method]
-
-        if (!methodFxn) {
-          throw new Error(`Unknown function ${method} for contract Vouching`)
-        }
-
-        let data = { transactions: [] }
-        const query = transactionQueries.allTransactionsQuery
-
-        const txId = nextTxId++
-
         try {
-          data = cache.readQuery({ query })
-          // console.log('existing data', data)
-        } catch (error) {
-          console.error(error)
-        }
+          const { contractName, method, args } = variables.txData
+          await getMetamaskPermissions()
+          const provider = new ethers.providers.Web3Provider(window.ethereum)
+          const network = await provider.getNetwork()
+          const signer = provider.getSigner()
+          const address = abiMapping.getAddress(contractName, network.chainId)
+          const abi = abiMapping.getAbi(contractName)
+          const contract = new ethers.Contract(address, abi, signer)
 
-        // const newArgs = Array.from(args)
-        // newArgs.__typename = 'JSON'
+          const methodFxn = contract[method]
 
-        const newArgs = {
-          values: Array.from(args).map(arg => arg.toString()),
-          __typename: 'JSON'
-        }
+          if (!methodFxn) {
+            throw new Error(`Unknown function ${method} for contract Vouching`)
+          }
 
-        const newTx = {
-          __typename: 'Transaction',
-          id: txId,
-          method,
-          completed: false,
-          sent: false,
-          hash: '',
-          error: '',
-          blockNumber: null,
-          args: newArgs
-        }
+          let data = { transactions: [] }
+          const query = transactionQueries.allTransactionsQuery
 
-        if (data.transactions) {
-          data.transactions.push(newTx)
-        } else {
-          data.transactions = [newTx]
-        }
+          const txId = nextTxId++
 
-        cache.writeQuery({ query, data })
+          try {
+            data = cache.readQuery({ query })
+            // console.log('existing data', data)
+          } catch (error) {
+            console.error(error)
+          }
 
-        const gasLimit = await contract.estimate[method](...args)
-        // Hack to ensure it works!
-        const newGasLimit = gasLimit.add(3000)
+          // const newArgs = Array.from(args)
+          // newArgs.__typename = 'JSON'
 
-        const id = `Transaction:${txId}`
-        const readTx = () => {
-          return cache.readFragment({ fragment: transactionQueries.transactionFragment, id })
-        }
+          const newArgs = {
+            values: Array.from(args).map(arg => arg.toString()),
+            __typename: 'JSON'
+          }
 
-        const transactionData = contract.interface.functions[method].encode(args)
-        const unsignedTransaction = {
-          data: transactionData,
-          to: contract.address,
-          gasLimit: newGasLimit
-        }
+          const newTx = {
+            __typename: 'Transaction',
+            id: txId,
+            method,
+            contractName,
+            completed: false,
+            sent: false,
+            hash: '',
+            error: '',
+            blockNumber: null,
+            args: newArgs
+          }
 
-        signer.sendUncheckedTransaction(unsignedTransaction)
-          .then(async function (hash) {
-            let transaction = readTx()
-            let data = { ...transaction, hash, sent: true }
-            cache.writeData({ id, data })
-            transaction = readTx()
+          if (data.transactions) {
+            data.transactions.push(newTx)
+          } else {
+            data.transactions = [newTx]
+          }
 
-            const receipt = await poll(() => {
-              return provider.getTransactionReceipt(hash).then(receipt => {
-                if (receipt === null) { return undefined }
+          cache.writeQuery({ query, data })
 
-                return receipt
+
+          const gasLimit = await contract.estimate[method](...args)
+          // Hack to ensure it works!
+          const newGasLimit = gasLimit.add(3000)
+
+          const id = `Transaction:${txId}`
+          const readTx = () => {
+            return cache.readFragment({ fragment: transactionQueries.transactionFragment, id })
+          }
+
+          const transactionData = contract.interface.functions[method].encode(args)
+          const unsignedTransaction = {
+            data: transactionData,
+            to: contract.address,
+            gasLimit: newGasLimit
+          }
+          signer.sendUncheckedTransaction(unsignedTransaction)
+            .then(async function (hash) {
+              let transaction = readTx()
+              let data = { ...transaction, hash, sent: true }
+              cache.writeData({ id, data })
+              transaction = readTx()
+
+              const receipt = await poll(() => {
+                return provider.getTransactionReceipt(hash).then(receipt => {
+                  if (receipt === null) { return undefined }
+
+                  return receipt
+                })
+              }, { onceBlock: provider }).catch(error => {
+                console.error(`Unable to get transaction receipt for tx with hash: ${hash} - `, error)
+                throw error
               })
-            }, { onceBlock: provider }).catch(error => {
-              console.error(`Unable to get transaction receipt for tx with hash: ${hash} - `, error)
-              throw error
+
+              if (receipt.status === 0) {
+                throw new Error(`Ethereum tx had a 0 status. Tx hash: ${hash}`)
+              }
+
+              data = { ...transaction, blockNumber: receipt.blockNumber, completed: true }
+              cache.writeData({ id, data })
             })
+            .catch(error => {
+              console.error(`Error occured while sending transaction`, error)
 
-            if (receipt.status === 0) {
-              throw new Error(`Ethereum tx had a 0 status. Tx hash: ${hash}`)
-            }
-
-            data = { ...transaction, blockNumber: receipt.blockNumber, completed: true }
-            cache.writeData({ id, data })
-          })
-          .catch(error => {
-            console.error(`Error occured while sending transaction`, error)
-
-            const transaction = readTx()
-            const data = { ...transaction, sent: true, completed: true, error: error.message }
-            cache.writeData({ id, data })
-          })
-
-        return null
+              const transaction = readTx()
+              const data = { ...transaction, sent: true, completed: true, error: error.message }
+              cache.writeData({ id, data })
+            })
+            
+          return newTx
+        } catch (error) {
+          console.error('sendTransaction: ', variables, error)
+          throw error
+        }
       }
     }
   }
