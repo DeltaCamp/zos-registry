4c142297e6e3a89aa7dfcdba284160fe9add8e3d
Brendan Asselstine <mail.asselstine@gmail.com>
2019-02-08 11:37:47 -0800

    Rebased onto master

---
+++ src/components/withTracker.jsx
@@ -0,0 +1,44 @@
+import React, { Component, } from "react";
+import GoogleAnalytics from "react-ga";
+
+if (!!process.env.REACT_APP_GA_TRACKING_ID) {
+  console.log('Initializing Google Analytics with tracking ID: ', process.env.REACT_APP_GA_TRACKING_ID)
+  GoogleAnalytics.initialize(process.env.REACT_APP_GA_TRACKING_ID)
+}
+
+export const withTracker = (WrappedComponent, options = {}) => {
+  const trackPage = page => {
+    if (!!process.env.REACT_APP_GA_TRACKING_ID) {
+      console.log('Tracking: ', page)
+      GoogleAnalytics.set({
+        page,
+        ...options,
+      });
+      GoogleAnalytics.pageview(page);
+    }
+  };
+
+  const HOC = class extends Component {
+    componentDidMount() {
+      const page = this.props.location.pathname + this.props.location.search
+      trackPage(page)
+    }
+
+    componentDidUpdate(prevProps) {
+      const currentPage =
+        prevProps.location.pathname + prevProps.location.search
+      const nextPage =
+        this.props.location.pathname + this.props.location.search
+
+      if (currentPage !== nextPage) {
+        trackPage(nextPage)
+      }
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />
+    }
+  }
+
+  return HOC
+}
