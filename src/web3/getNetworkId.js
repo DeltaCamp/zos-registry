39ac840c28eba36b90b866f1af48917e48ad71ae
Brendan Asselstine <mail.asselstine@gmail.com>
2019-02-11 09:30:09 -0800

    Fixed eslint

--- src/web3/getNetworkId.js
+++ src/web3/getNetworkId.js
@@ -1,7 +1,7 @@
 import { getNetworkName } from '~/web3/getNetworkName'
 import { ethers } from 'ethers'
 
-export async function getNetworkId() {
+export async function getNetworkId () {
   const network = ethers.utils.getNetwork(await getNetworkName())
   if (network) {
     return network.chainId
