69fa91d12fcde6b553e9bb9f10a27a226bbf6818
Brendan Asselstine <mail.asselstine@gmail.com>
2019-02-12 12:58:11 -0800

    Fixed test

--- src/utils/__tests__/dateRelative.test.js
+++ src/utils/__tests__/dateRelative.test.js
@@ -7,7 +7,7 @@ describe('dateRelative', () => {
   it('should return the relative date', () => {
     tk.freeze(FROZEN_TIME)
 
-    expect(dateRelative(1549997174543, FROZEN_TIME)).toEqual('today at 10:46 AM')
+    expect(dateRelative(1549997174543, FROZEN_TIME)).toEqual(expect.stringMatching(/today at \d{1,2}:46 (AM|PM)/))
 
     tk.reset()
   })
@@ -15,7 +15,7 @@ describe('dateRelative', () => {
   it('should use the default current date as the future date if non supplied', () => {
     tk.freeze(FROZEN_TIME)
 
-    expect(dateRelative(FROZEN_TIME - 100000000)).toEqual('yesterday at 7:02 AM')
+    expect(dateRelative(FROZEN_TIME - 100000000)).toEqual(expect.stringMatching(/yesterday at \d{1,2}:02 (AM|PM)/))
 
     tk.reset()
   })
